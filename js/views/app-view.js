e7067a7052bf49cab4876e38ac0d1a4de5d0d4f5
SG <sg@doodle.com>
2017-09-10 14:48:18 +0300

    First commit using simple flexbox (not Bootstrap's flex)

---
+++ js/views/app-view.js
@@ -0,0 +1,101 @@
+/*global Backbone, jQuery, _ */
+var app = app || {};
+
+(function ($) {
+	'use strict';
+
+	// Our overall **AppView** is the top-level piece of UI.
+	app.AppView = Backbone.View.extend({
+		// Instead of generating a new element, bind to the existing skeleton of
+		// the App already present in the HTML.
+		el: '#cv-app',
+
+		// Delegated events for creating new items, and clearing completed ones.
+		events: {
+			'click .section': 'openSection'
+		},
+
+		// At initialization we bind to the relevant events on the `Todos`
+		// collection, when items are added or changed. Kick things off by
+		// loading any preexisting todos that might be saved in *localStorage*.
+		initialize: function () {
+			this.allCheckbox = this.$('#toggle-all')[0];
+			this.$input = this.$('#new-todo');
+			this.$footer = this.$('#footer');
+			this.$main = this.$('#main');
+			this.$list = $('#todo-list');
+
+			this.listenTo(app.todos, 'add', this.addOne);
+			this.listenTo(app.todos, 'reset', this.addAll);
+			this.listenTo(app.todos, 'change:completed', this.filterOne);
+			this.listenTo(app.todos, 'filter', this.filterAll);
+			this.listenTo(app.todos, 'all', this.render);
+
+			// Suppresses 'add' events with {reset: true} and prevents the app view
+			// from being re-rendered for every model. Only renders when the 'reset'
+			// event is triggered at the end of the fetch.
+			app.todos.fetch({reset: true});
+		},
+
+		// Re-rendering the App just means refreshing the statistics -- the rest
+		// of the app doesn't change.
+		render: function () {
+			var completed = app.todos.completed().length;
+			var remaining = app.todos.remaining().length;
+
+			if (app.todos.length) {
+				this.$main.show();
+				this.$footer.show();
+
+				this.$footer.html(this.statsTemplate({
+					completed: completed,
+					remaining: remaining
+				}));
+
+				this.$('#filters li a')
+					.removeClass('selected')
+					.filter('[href="#/' + (app.TodoFilter || '') + '"]')
+					.addClass('selected');
+			} else {
+				this.$main.hide();
+				this.$footer.hide();
+			}
+
+			this.allCheckbox.checked = !remaining;
+		},
+
+		// Add a single todo item to the list by creating a view for it, and
+		// appending its element to the `<ul>`.
+		addOne: function (todo) {
+			var view = new app.TodoView({ model: todo });
+			this.$list.append(view.render().el);
+		},
+
+		// Add all items in the **Todos** collection at once.
+		addAll: function () {
+			this.$list.html('');
+			app.todos.each(this.addOne, this);
+		},
+
+		filterOne: function (todo) {
+			todo.trigger('visible');
+		},
+
+		filterAll: function () {
+			app.todos.each(this.filterOne, this);
+		},
+
+		// Generate the attributes for a new Todo item.
+		newAttributes: function () {
+			return {
+				title: this.$input.val().trim(),
+				order: app.todos.nextOrder(),
+				completed: false
+			};
+		},
+
+		openSection: function () {
+			return false;
+		}
+	});
+})(jQuery);
